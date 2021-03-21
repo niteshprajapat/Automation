a5ac7db7c376a060f75375dc9848df9339be8f2e
Nitesh Prajapat <nitesh.19jdml019@jietjodhpur.ac.in>
2021-03-21 23:30:37 +0530

    pipelines created

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from time import sleep
+import pyautogui
+
 
 chromedriver_path = 'C:/Users/Nitesh Prajapati/Downloads/chromedriver.exe' 
 driver = webdriver.Chrome(executable_path=chromedriver_path)
@@ -11,7 +14,8 @@ sleep(5)
 ############################################### 
 
 def falcon_9():
-    pass
+    falcon_btn = driver.find_element(By.LINK_TEXT,"Falcon 9").click()
+    sleep(4)
 
 ################################################
 
