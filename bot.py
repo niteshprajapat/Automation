b78f3a44b8c02ce21c4335d637dd389c745868ea
Nitesh Prajapat <nitesh.19jdml019@jietjodhpur.ac.in>
2021-03-28 01:40:16 +0530

    medium blog automation

--- bot.py
+++ bot.py
@@ -7,38 +7,61 @@ import pyautogui
 
 chromedriver_path = 'C:/Users/Nitesh Prajapati/Downloads/chromedriver.exe' 
 driver = webdriver.Chrome(executable_path=chromedriver_path)
-driver.get("https://www.spacex.com")
 
-sleep(5)
+def medium_blog():
+    try:
+        sleep(5)
+        driver.get("https://medium.com/")
+        driver.maximize_window()
+        sleep(2)
 
-############################################### 
+        sign_in = driver.find_element(By.LINK_TEXT, "Sign In")
+        sign_in.click()
 
-def falcon_9():
-    falcon_btn = driver.find_element(By.LINK_TEXT,"Falcon 9").click()
-    sleep(4)
+        ask = pyautogui.prompt("Do you have an account ?")
 
-################################################
+        if ask == "No" or ask == "no" or ask == "NO":
+            ask_query = pyautogui.prompt("Do you want to create new one ? ")
+            if ask_query == "YES" or ask_query == "yes" or ask_query == "Yes":
+                create_acc = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div/div[3]/div[6]/div/p/button/b')
+                create_acc.click()
 
-def falcon_heavy():
-    pass
+                choice = pyautogui.prompt("Select any one ......")
+                if choice == 'email':
+                    email_option = driver.find_element(By.ID, 'email-susi-button-text')
+                    email_option.click()
+                    sleep(4)
+
+                    your_email_address = pyautogui.prompt("Your Email Address.......")
+                    sleep(2)
+
+                    email_type = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[2]/div/div/div[2]/div/p/input')
+                    email_type.send_keys(your_email_address, Keys.ENTER)
 
 
-################################################
+                    ############## reCaptcha experiment #############
 
-def dragon():
-    pass
+                    # while True:
+                    #     generate_captcha = pyautogui.prompt("Want to generate new re-Captcha code ?? ")
+                    #     if generate_captcha == "yes":
+                    #         driver.find_element(By.ID, 'recaptcha-reload-button').click()
+                    #         sleep(1)
+                    #     elif not (generate_captcha == "yes"):
+                    #         # driver.close()
+                    #         pass
 
-################################################
+                    pyautogui.prompt("Please Solve the reCaptcha puzzle..... and Continue ...")        
+                            
+                    OK = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/div[1]/div/div[1]/div[3]/div/button').click()
+                    
 
-def starship():
-    pass
+                    #################################
+                    
+        sleep(5)
 
-################################################
+        # driver.quit()
 
-def human_spaceflight():
-    pass
+    except Exception as e:
+        print("Error Found!!")
 
-#################################################
-
-def rideshare():
-    pass
+medium_blog()
